cfab933603dbc0df9a9707226215d6b9c76475c5
dahneal <56851005+dahneal@users.noreply.github.com>
2019-11-10 09:45:43 -0800

    Update hashmap.cpp

--- hashmap.cpp
+++ hashmap.cpp
@@ -8,14 +8,31 @@
 class HashMap{
 	private: 
 	std::map<std::string,Report> m;
+	std::map<std::string,Report> m1;
+	std::map<std::string,Report> m2;
+	std::map<std::string,Report> m2;
+	std::map<std::string,Report> m3;
+	std::map<std::string,Report> m4;
 	//string *head;
 	
 	public:
 	void Insert(string hash, Report report)
 	{
 		m.emplace(hash, report);
+		m1.emplace(hash, report);
+		m2.emplace(hash, report);
+		m3.emplace(hash, report);
+		m4.emplace(hash, report);
 	//	head = *hash;
 	
+	}
+	void checkHashes();
+	{
+		if (m.end() != m1.end() ||m.end() != m2.end() ||m.end() != m3.end() ||m.end() != m4.end())
+		{
+			cout << "FATAL ERROR: HASH NOT MATCHING\n\n\n\n";
+			exit;
+		}
 	}
 	/*
 	Report* Search(string hash)
