ee375cf77a4fe2ed5981ee75c1f2dfa895b59a7c
dahneal <56851005+dahneal@users.noreply.github.com>
2019-12-01 14:16:12 -0800

    Update Keys.h

--- Keys.h
+++ Keys.h
@@ -10,13 +10,16 @@ template <class DataType>
 class Keys
 {
 public:
-	void generateEncryptionKey(std::string encryptedPass); //Two different keys will be made using this hashing method
-	long int publicKey[50];
+	void generateEncryptionKey(std::string hash); //Two different keys will be made using this hashing method
+	
+	long int getPrivateKey() {return publicKey;}
+	long int getPublicKey() {return encryped;}
 
 private:
 	
 	long int keyModulus, totientFunction, i, flag; // i is just any iterator
 	long int publicKeyInverse[50], temp[50], maxValOfKey;
+	long int publicKey[50];
 	char encrypted[50], decrypted[50]; // encrypted = private key
 	long int cd(long int a);
 	long int prime(long int pr);
