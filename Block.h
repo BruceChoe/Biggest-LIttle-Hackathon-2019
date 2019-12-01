7e3b02515f79590361ec92f4ccff6876c11d844b
dahneal <56851005+dahneal@users.noreply.github.com>
2019-12-01 12:11:06 -0800

    Update Block.h

--- Block.h
+++ Block.h
@@ -5,32 +5,28 @@
 #include <iostream>
 #include <cstdint>
 #include <sstream>
-#include "Report.h"
-#include "FI.h"
-#include "ThirdParty.h"
+
 
 using namespace std;
 /*
 	Creation of a block: enter private key, info, create.
 */
+template <class DataType>
 class Block {
 public:
 	string sPrevHash;
-	//Block();
-	Block(uint32_t nIndexIn, Report& rReportDocsIn);
-	Block(uint32_t nIndexIn, FI& fiID, ThirdParty& tpID, Report& rReportDocsIn);
+	
+	Block(uint32_t nIndexIn, DataType& inputData);
 	string GetHash();
 	void MineBlock();
 	uint32_t getIndex();
-	Report& getReport();
+	DataType& getData();
 private:
 	uint32_t _nIndex;
 	int64_t _nNonce;
 	string _sHash;
 	time_t _tTime;
-	FI& fiID;
-	ThirdParty& tpID;
-	Report& reportDocs;
+	DataType& data;
 
 	string _CalculateHash() const;
 };
