2fccf17332134a21e8a8153b140ced2373f5beb7
dahneal <56851005+dahneal@users.noreply.github.com>
2019-12-01 12:39:38 -0800

    Update PasswordManager.cpp

--- PasswordManager.cpp
+++ PasswordManager.cpp
@@ -4,138 +4,14 @@
 #include <ctime>
 #include <iostream>
 
-PasswordManager::PasswordManager()
-{
-	readFile();
-}
-
-PasswordManager::~PasswordManager()
-{
-	writeFile();
-}
-
-bool PasswordManager::firstTimeLogin(std::string user, std::string pass)
-{
-	addUser(user, pass);
-	return login(user, pass);
-}
-
-bool PasswordManager::login(std::string user, std::string pass)
-{
-	auto givenUser = allUserData.find(user);
-	if (givenUser != allUserData.end())
-	{
-		std::string userSalt = givenUser->second.salt;
-		std::string givenPass = encryptPassword(pass, userSalt);
-
-		if (givenPass == givenUser->second.encryptedPassword) return true;
-	}
-	return false;
-}
-
-void PasswordManager::addUser(std::string user, std::string pass)
-{
-	std::string salt = randomSalt();
-	std::string encpass = encryptPassword(pass, salt);
-
-	addUser(user, salt, encpass);
-}
-
-void PasswordManager::addUser(std::string user, std::string salt, std::string encryptedPassword)
-{
-	PassInfo info(salt, encryptedPassword);
-	allUserData.insert(std::pair<std::string, PassInfo>(user, info));
-}
-
-void PasswordManager::removeUser(std::string user)
-{
-	auto givenUser = allUserData.find(user);
-	if (givenUser != allUserData.end())
-	{
-		allUserData.erase(givenUser);
-	}
-}
-
-void PasswordManager::printAllUsers()
-{
-	for (auto it = allUserData.begin(); it != allUserData.end(); it++)
-	{
-		std::cout << it->first << " " << it->second << "\n";
-	}
-}
-
-
-
-void PasswordManager::readFile()
-{
-	std::ifstream file(FILENAME);
-
-	std::string user;
-	std::string salt;
-	std::string encpass;
-
-	while (file >> user >> salt >> encpass)
-	{
-
-		std::cout << user << ' ' << salt << ' ' << encpass << std::endl;
-		addUser(user, salt, encpass);
-	}
-
-	file.close();
-}
-
-void PasswordManager::writeFile()
-{
-	std::ofstream file(FILENAME);
-
-	for (auto it = allUserData.begin(); it != allUserData.end(); it++)
-	{
-		file << it->first << " " << it->second << "\n";
-	}
-
-	file.close();
-}
-
-std::string PasswordManager::encryptPassword(std::string password, std::string salt)
-{
-	std::string result;
-	std::string input(password);
-	input += salt;
-
-	std::vector<unsigned char> hash(picosha2::k_digest_size);
-	picosha2::hash256_hex_string(input, result);
-
-	return result;
-}
-
-std::string PasswordManager::randomSalt()
-{
-	srand(time(nullptr));
-	std::string salt;
-	char ch;
-
-	for (int i = 0; i < 8; i++)
-	{
-		ch = static_cast<char>(rand() % 255);
-		salt += ch;
-	}
-
-	return salt;
-}
-
-std::ostream& operator<<(std::ostream& os, PassInfo& info)
-{
-	os << info.salt << ' ' << info.encryptedPassword;
-	return os;
-}
-
-void PasswordManager::generateEncryptionKey(std::string encryptedPass)
+template <class DataType>
+void PasswordManager<DataType>::generateEncryptionKey(std::string hash)
 {
 	long int firstPrime, secondPrime;
 	firstPrime = 12809;
 	secondPrime = 18371;
-	for (i = 0; encryptedPass[i] != NULL; i++)
-		encryptedPass[i] = encryptedPass[i];
+	for (i = 0; hash[i] != NULL; i++)
+		hash[i] = hash[i];
 	keyModulus = firstPrime * secondPrime;
 	totientFunction = (firstPrime - 1) * (secondPrime - 1);
 
@@ -162,7 +38,7 @@ void PasswordManager::generateEncryptionKey(std::string encryptedPass)
 	long int pt, ct, actualKey, len;
 	key = publicKey[0];
 	i = 0;
-	len = encryptedPass.length();
+	len = hash.length();
 
 	while (i != len)
 	{
@@ -198,7 +74,10 @@ void PasswordManager::generateEncryptionKey(std::string encryptedPass)
 	decrypted[i] = -1;
 
 }//end generateEncryptionKey
-long int PasswordManager::cd(long int a)
+
+
+template <class DataType>
+long int PasswordManager<DataType>::cd(long int a)
 {
 	long int key = 1;
 	while (1)
@@ -208,7 +87,9 @@ long int PasswordManager::cd(long int a)
 			return(key / a);
 	}
 }
-long int PasswordManager::prime(long int pr)
+
+template <class DataType>
+long int PasswordManager<DataType>::prime(long int pr)
 {
 	long int i;
 	maxValOfKey = sqrt(pr);
