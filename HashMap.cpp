f00a2b01a08a4b70870468b816ca4336e10eb106
dahneal <56851005+dahneal@users.noreply.github.com>
2019-12-01 12:24:34 -0800

    Update HashMap.cpp

--- HashMap.cpp
+++ HashMap.cpp
@@ -5,48 +5,27 @@
 #include <string>
 #include "Block.h"
 
+template <class DataType>
 class HashMap {
 private:
-	std::map<std::string, Report> m;
-	//string *head;
+	std::map<std::string, DataType> m; //m is the map
+	string tail; 
 
 public:
 	void Insert(string hash, Report report)
 	{
 		m.emplace(hash, report);
-		//	head = *hash;
+		tail = hash;
 
 	}
-	/*
-	Report* Search(string hash)
-	{
-		auto location = m.find(hash);
-		//Report *r = location.second;
-		auto temp = m.at[location];
-		return temp->second;
-	}
-	*/
-	Report* SearchByID(int FI_ID)
-	{
-		auto it = m.begin();
-		// Iterate through the map
-		while (it != m.end())
-		{
-			// Check if value of this entry matches with given value
-			if (it->second.getFiID() == FI_ID)
-			{
-				return &(it->second);
-			}
-			// Go to next entry in map
-			it++;
-		}
-	}
+
 	void printMap(ostream& os)
 	{
-		std::map<std::string, Report>::iterator it = m.begin();
+		std::map<std::string, DataType>::iterator it = m.begin();
 		while (it != m.end()) {
-			it->second.printReport(os);
+			os << it->second.DataType << " ";
 			it++;
 		}
+		os << "\n";
 	}
 };
